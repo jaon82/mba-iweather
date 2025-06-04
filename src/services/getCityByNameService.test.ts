f85a4a3568bf6f8d9d72303aad0b7d3e2a5f513d
Jean Nunes <jean.nunes@uzzipay.com>
2025-06-03 20:55:50 -0400

    Create mocks dir

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,20 +1,9 @@
+import { mockCityAPIResponse } from "@tests/mocks/mockCityAPIResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 describe("API: getCityByNameService", () => {
   it("should return city details", async () => {
-    const data = {
-      id: "1",
-      name: "São Paulo",
-      sys: {
-        country: "BR",
-      },
-      coord: {
-        lat: 123,
-        lon: 456,
-      },
-    };
-
-    jest.spyOn(api, "get").mockResolvedValue({ data });
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
 
     const response = await getCityByNameService("São Paulo");
 
